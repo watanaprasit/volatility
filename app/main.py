a62ae970709432b0d9cbfbd31261ede4ad3b9118
daryl <watanaprasit@gmail.com>
2025-01-03 04:03:50 +0800

    changes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+import pandas as pd
+from fastapi.responses import JSONResponse
+import os
 
 app = FastAPI()
 
+# Define the path to your CSV file
+csv_file_path = os.path.join("scripts", "static", "merged_crude_oil_ovx_data.csv")
+
 @app.get("/")
-async def root():
-    return {"message": "Hello, FastAPI!"}
+async def read_csv():
+    # Check if the CSV file exists
+    if os.path.exists(csv_file_path):
+        # Read the CSV file into a DataFrame
+        data = pd.read_csv(csv_file_path)
+
+        # Convert the DataFrame to a dictionary and return it as JSON
+        data_dict = data.to_dict(orient="records")
+        return JSONResponse(content=data_dict)
+    else:
+        return {"message": "CSV file not found."}
+
